1c7916980afe640e786ef43913ed1ff91006ace5
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-06-14 18:55:41 +0530

    created auth middleware

---
+++ server/middlewares/authMiddleware.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const User = require('../collections/userCollection')
+
+const protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startWith('Bearer')){
+        try {
+            // Get token from headers
+            token = req.headers.authorization.split(' ')[1]
+
+            // Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+            // Get user from token, also removed password
+            req.user = await User.findById(decoded.id).select('-password')
+
+            next()
+        } catch (error) {
+            console.log('Error', error)
+            return res.status(401).json({ error: 'Not authorized'})
+        }
+    }
+
+    if (!token) {
+        return res.status(401).json({ error: 'Not authorized, no token'})
+    }
+}
+
+module.exports = protect
