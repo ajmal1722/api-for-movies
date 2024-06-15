ecec586b55f7fb8f84204aa3485c2a30d3be8cde
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-06-15 17:09:32 +0530

    swagger documentation for user schema

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,6 +3,39 @@ const router = express.Router()
 const userController = require('../controller/userController');
 const { protect  } = require('../middlewares/authMiddleware')
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Users:
+ *       type: object
+ *       required:
+ *         - name
+ *         - email
+ *         - password
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the movie
+ *         name:
+ *           type: string
+ *           description: The name of the user
+ *         email:
+ *           type: string
+ *           description: The email of user
+ *         isAdmin:
+ *           type: boolean
+ *           description: The admin status of the user
+ *         password:
+ *           type: string
+ *           description: The password of the user
+ *       example:
+ *         id: "60d0fe4f5311236168a109ca"
+ *         name: "John Doe"
+ *         email: "john@gmail.com"
+ *         isAdmin: false
+ *         password: "$2a$12$0zCk8d6IVtX.FF1bXaWVs.dlIqFqYc2pL4lHZK9Z5NP8QGPe2oJGy"
+ */
 router.get('/', userController.homeRoute);
 router.get('/my-account', protect, userController.myAccount);
 router.post('/user/signup', userController.userSignUp);
